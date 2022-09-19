0ecac6c13d5e0e916265f8caa406a170fbb65bce
Axel <eldrank@gmail.com>
2022-09-19 10:27:29 +0200

    make store function

--- src/index.js
+++ src/index.js
@@ -11,3 +11,9 @@ class toDo {
 
 const newTodo = new toDo("titre", "descriptif", "19.09.22", "maximum");
 console.log(newTodo);
+
+const storeToDo = (() => {
+    toDoStorage.push(newTodo)
+})()
+
+console.log(toDoStorage[0])
