f8ac89c315ed57a0b1082cba3c152f57c2a5b7f1
Axel <eldrank@gmail.com>
2022-09-19 14:31:30 +0200

    make display of toDo infos

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
-let toDoStorage = [];
+let toDoStore = [];
 
-class toDo {
+class toDo{
     constructor(title, description, dueDate, priority){
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+
+        return {title, description, dueDate, priority}
     }
 }
 
-const newTodo = new toDo("titre", "descriptif", "19.09.22", "maximum");
-console.log(newTodo);
+(function storeToDo(){
+    const newTodo = new toDo("titre", "descriptif", "19.09.22", "maximum");
+    toDoStore.push(newTodo);
 
-const storeToDo = (() => {
-    toDoStorage.push(newTodo)
-})()
+    displayStore();
+})();
 
-console.log(toDoStorage[0])
+function displayStore(){
+    const container = document.querySelector(".container");
+
+    toDoStore.forEach(tasks => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        container.appendChild(card);
+
+        for (let eachTask in tasks){
+            const txt = document.createElement("p");
+            txt.textContent = `${eachTask}: ${tasks[eachTask]}`;
+            card.appendChild(txt);
+        }
+    });
+}
+
+console.log(toDoStore[0]);
+
