9f270ad8e9d8d5e88f2831e7a99df6bc38c65f8c
Axel <eldrank@gmail.com>
2022-09-19 10:22:57 +0200

    make class constructor

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-console.log(document.querySelector("h1"));
+let toDoStorage = [];
 
+class toDo {
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+const newTodo = new toDo("titre", "descriptif", "19.09.22", "maximum");
+console.log(newTodo);
