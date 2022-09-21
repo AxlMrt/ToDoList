bf200574da3a661222675b843dae7a2b55f41845
Axel <eldrank@gmail.com>
2022-09-21 08:56:42 +0200

    make edit & delete button work for each card

--- src/index.js
+++ src/index.js
@@ -12,22 +12,12 @@ class Task{
 }
 
 // FUNCTIONS //
-function userName(){
-    const nameInput = document.getElementById("nameInput");
-    const username = localStorage.getItem("username") || "";
-
-    nameInput.value = username;
-
-    nameInput.addEventListener("change", e => {
-        localStorage.setItem("username", e.target.value);
-    })
-}
-
 function pushTasks(){
     const title = document.getElementById("title").value;
     const inputDate = new Date();
 
     document.getElementById("title").value = "";
+    document.querySelectorAll("label input").value = "";
 
     const newTask = new Task(title, inputDate);
     storeTasks.push(newTask);
@@ -57,32 +47,8 @@ function displayTasks(){
 
         content.innerHTML = `<input type=text value="${tasks.title}" readonly>`;
         editBtn.textContent = "Edit";
-        dltBtn.textContent = "Delete"
-  
-        container.appendChild(card);
-        card.appendChild(checkBox);
-        card.appendChild(content)
-        card.appendChild(editBtn);
-        card.appendChild(dltBtn)
+        dltBtn.textContent = "Delete";
 
-        editTasks()
-        removeTasks()
-    });
-}
-
-function removeTasks(){
-    const dltBtn = document.querySelector(".dltBtn")
-    dltBtn.addEventListener("click", function() {
-            storeTasks.splice(this.parentElement.getAttribute("data-index"), 1);
-            this.parentElement.remove();
-            localStorage.setItem("storeTasks", JSON.stringify(storeTasks));
-    });
-}
-
-function editTasks(){
-    const content = document.querySelector(".todoContent");
-    const editBtn = document.querySelector(".editBtn");
-    storeTasks.forEach(tasks => {
         editBtn.addEventListener("click", e => {
             const input = content.querySelector("input")
             input.removeAttribute("readonly");
@@ -91,14 +57,50 @@ function editTasks(){
                 input.setAttribute("readonly", true);
                 tasks.title = e.target.value;
                 localStorage.setItem("storeTasks", JSON.stringify(storeTasks));
-                displayTasks()
+                displayTasks();
             })
         })
+
+        dltBtn.addEventListener('click', (e) => {
+			storeTasks = storeTasks.filter(t => t != tasks);
+			localStorage.setItem('storeTasks', JSON.stringify(storeTasks));
+			displayTasks();
+		})
+  
+        container.appendChild(card);
+        card.appendChild(checkBox);
+        card.appendChild(content)
+        card.appendChild(editBtn);
+        card.appendChild(dltBtn)
+
+    });
+}
+
+function editTasks(){
+    const content = document.querySelector(".todoContent");
+    const editBtn = document.querySelector(".editBtn");
+    storeTasks.forEach(tasks => {
+        
     })
 }
 
 function handleForm(event){
     event.preventDefault();
+}
+
+function userName(){
+    const nameInput = document.getElementById("nameInput");
+    const username = localStorage.getItem("username") || "";
+
+    nameInput.value = username;
+
+    nameInput.addEventListener("change", e => {
+        localStorage.setItem("username", e.target.value);
+    })
+}
+
+function priorityCheck(){
+
 }
 
 // ADD EVENT LISTENER //
