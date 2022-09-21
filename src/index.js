0998307a7b6b9941baf5661f6b6f14eb69dec91a
Axel <eldrank@gmail.com>
2022-09-21 22:57:18 +0200

    Add priority to object

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ export let storeTasks = JSON.parse(localStorage.getItem("storeTasks")) || [];
 
 // CLASS //
 class Task{
-    constructor(title, inputDate, dueDate){
+    constructor(title, inputDate, dueDate, priority){
         this.title = title;
         this.inputDate = inputDate;
         this.dueDate = dueDate;
+        this.priority = priority;
     }
 }
 
@@ -21,20 +22,39 @@ function pushTasks(){
     const title = document.getElementById("title").value;
     const dueDate = document.getElementById("dueDate").value;
     const inputDate = new Date();
+    
+    let priority = "";
+
+    if (document.getElementById("low").checked === true){
+        priority = document.getElementById("low").value;
+    }
+    if (document.getElementById("middle").checked === true){
+        priority = document.getElementById("middle").value;
+    }
+    if (document.getElementById("high").checked === true){
+        priority = document.getElementById("high").value;
+    }
 
     document.getElementById("title").value = "";
+    document.getElementById("dueDate").value = "";
+    document.getElementById("low").checked = false;
+    document.getElementById("middle").checked = false;
+    document.getElementById("high").checked = false;
 
     if(title === "" || dueDate === "") {
         return alert("Vous devez remplir les champs.")
     }
 
-    const newTask = new Task(title, inputDate, dueDate);
+    const newTask = new Task(title, inputDate, dueDate, priority);
 
     storeTasks.push(newTask);
     localStorage.setItem("storeTasks", JSON.stringify(storeTasks));
 
     displayTasks();
 };
+
+
+
 
 function handleForm(event){
     event.preventDefault();
